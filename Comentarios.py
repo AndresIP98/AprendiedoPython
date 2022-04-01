302502945c311ecac4d1bb2995bc3db9177c14f7
Andrés Pineda <101218453+AndresIP98@users.noreply.github.com>
2022-04-01 18:01:30 -0500

    Update Comentarios.py

--- Comentarios.py
+++ Comentarios.py
@@ -1,6 +1,6 @@
 print("Hola, estos son comentarios en py")
 #Esto es un comentario
-"Esto es otro comentario""
+"Esto es otro comentario"
 '''Hola esto es un comentario multilinea o largo'''
 """Asi tambien seria un comentario multilinea"""
 """
